3bf9e917589ceb171ae07bee24dfa491323eeb75
bubonik0610@gmail.com <64885753+bubonik1505@users.noreply.github.com>
2020-08-26 17:43:59 +0700

    add model Category

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    '''Класс моделей. Создает соответствующую таблицу в базе данных'''
+    name = models.CharField(verbose_name='Имя', max_length=100)
+    slug = models.SlugField(verbose_name='url', max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    #для корректного перевода на русский класса Category используем подкласс Meta
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
